e26782031cddf0796ac61318e072564b35416e95
Daniele Gazzola <danielegazzola2000@gmail.com>
2023-06-08 15:12:06 +0200

    Test 6

--- script/Build.sh
+++ script/Build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-g++ -o test1 googletest/src/gtest_main.cc googletest/samples/sample1_unittest.cc googletest/samples/sample2_unittest.cc googletest/samples/sample3_unittest.cc googletest/samples/sample4_unittest.cc googletest/samples/sample5_unittest.cc googletest/samples/sample9_unittest.cc googletest/samples/sample1.cc googletest/samples/sample2.cc googletest/samples/sample4.cc -lgtest -lgtest_main -lpthread
+g++ -o test1 googletest/src/gtest_main.cc googletest/samples/sample1_unittest.cc googletest/samples/sample2_unittest.cc googletest/samples/sample3_unittest.cc googletest/samples/sample4_unittest.cc googletest/samples/sample5_unittest.cc googletest/samples/sample6_unittest.cc googletest/samples/sample1.cc googletest/samples/sample2.cc googletest/samples/sample4.cc -lgtest -lgtest_main -lpthread
 ./test1
 
 #g++ -o test10 googletest/samples/sample10_unittest.cc googletest/samples/sample1.cc googletest/samples/sample2.cc googletest/samples/sample4.cc -lgtest -lgtest_main -lpthread
 #./test10
 
-# googletest/samples/sample6_unittest.cc googletest/samples/sample7_unittest.cc googletest/samples/sample8_unittest.cc 
+# googletest/samples/sample7_unittest.cc googletest/samples/sample8_unittest.cc googletest/samples/sample9_unittest.cc 
