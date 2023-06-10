c0d4cdae681ddb0a68688edc525e7922a92a748e
Daniele Gazzola <danielegazzola2000@gmail.com>
2023-06-10 10:34:19 +0200

    Saturday

---
+++ premake4.lua
@@ -0,0 +1,19 @@
+workspace "Project"
+    configurations { "Debug", "Release" }
+
+project "Project"
+    location "Project"
+    kind "ConsoleApp"
+    language "C++"
+    
+    files { "googletest/src/gtest_main.cc" }
+
+    configuartion "Debug"
+        defines { "DEBUG" }
+        symbols "On"
+        targetdir "bin/%{cfg.buildcfg}"
+
+    configuartion "Release"
+        defines { "NDEBUG" }
+        symbols "On"
+        targetdir "bin/%{cfg.buildcfg}"
