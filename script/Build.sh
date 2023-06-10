53c53aaad1481fdeec9d5b3e2bf9ccc0a142836d
Daniele Gazzola <danielegazzola2000@gmail.com>
2023-06-10 10:14:50 +0200

    Saturday

--- script/Build.sh
+++ script/Build.sh
@@ -2,6 +2,4 @@
 
 g++ -o test googletest/src/gtest_main.cc googletest/samples/sample1_unittest.cc googletest/samples/sample2_unittest.cc googletest/samples/sample3_unittest.cc googletest/samples/sample4_unittest.cc googletest/samples/sample5_unittest.cc googletest/samples/sample6_unittest.cc googletest/samples/sample7_unittest.cc googletest/samples/sample8_unittest.cc googletest/samples/sample10_unittest.cc googletest/samples/sample1.cc googletest/samples/sample2.cc googletest/samples/sample4.cc -lgtest -lgtest_main -lpthread
 
-./test
-
 #googletest/samples/sample9_unittest.cc 
